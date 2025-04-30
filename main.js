edb05b955f2e7d30226d6b1af1b3778bbca675b5
Arweqah-Com <a.adel@arweqah.sa>
2025-04-30 16:21:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-function checkPassword() {
-  const password = prompt("Please enter the password:");
-
-  // You can change this password as needed
-  if (password === "1234") {
-    document.querySelector("button").style.display = "none";
-    document.getElementById("dashboard").style.display = "block";
-  } else {
-    alert("Incorrect password!");
-  }
+function showDashboard() {
+  document.querySelector("button").style.display = "none";
+  document.getElementById("dashboard").style.display = "block";
 }
