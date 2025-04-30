0db32cf022174bfa9bc698550d533b7ce3857d98
Arweqah-Com <a.adel@arweqah.sa>
2025-04-30 16:32:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function showDashboard() {
   document.querySelector("button").style.display = "none";
   document.getElementById("dashboard").style.display = "block";
 }
+
+function goBack() {
+  document.querySelector("button").style.display = "block";
+  document.getElementById("dashboard").style.display = "none";
+}
